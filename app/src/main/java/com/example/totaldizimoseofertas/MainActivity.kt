c2a157aeddbe6df7b21961dc1d83b85526c84c0f
IsaacYusuke <isaacyusuke@usp.br>
2023-12-20 13:40:32 -0300

    Estrutura básica

--- app/src/main/java/com/example/totaldizimoseofertas/MainActivity.kt
+++ app/src/main/java/com/example/totaldizimoseofertas/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.totaldizimoseofertas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.totaldizimoseofertas.ui.theme.TotalDizimosEOfertasTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +28,32 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.surface
+                    //color = com.example.totaldizimoseofertas.ui.theme.Purple80
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize()) {
+
+                        Row(modifier = Modifier.fillMaxWidth()) {
+
+                            Column(modifier = Modifier.weight(1f),
+                                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                                Text( text = "Notas",
+                                    fontSize =30.sp)
+
+                            }
+
+                            Column(modifier = Modifier.weight(1f),
+                                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                                Text( text = "Moedas",
+                                    fontSize =30.sp)
+
+                            }
+
+
+                        }
+                    }
                 }
             }
         }
